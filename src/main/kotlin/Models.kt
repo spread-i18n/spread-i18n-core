7e844fedc35189420fa498f7fff2746830ffb993
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-04-30 13:23:08 +0200

    Move 'Source/Target' related class from Models.kt to SourceTargetMatching.kt file

--- src/main/kotlin/Models.kt
+++ src/main/kotlin/Models.kt
@@ -1,51 +1,10 @@
-import Locales.Companion.allLocales
-import java.io.Closeable
-import java.io.File
 import java.nio.file.Path
-import java.text.DateFormat
-import java.util.*
 
 fun <T> Sequence<T>.skipTo(n: Int): Sequence<T> = drop(n)
 
-fun Locale.identifiedBy(localeDataCandidate: String): Boolean {
-    return when (localeDataCandidate.toLowerCase()) {
-        this.country.toLowerCase() -> true
-        this.displayCountry.toLowerCase() -> true
-        this.language.toLowerCase() -> true
-        this.displayLanguage.toLowerCase() -> true
-        this.toLanguageTag().toLowerCase() -> true
-        else -> false
-    }
-}
-
-class Locales {
-
-    fun findLocale(localeDataCandidate: String): Locale? {
-        return items.find { locale -> locale.identifiedBy(localeDataCandidate) }
-    }
-
-    private constructor()
-
-    companion object {
-        val allLocales = Locales()
-    }
-
-    val items: Array<Locale> by lazy {
-        DateFormat.getAvailableLocales()
-    }
-}
 
 open class ImportException(message: String): Exception(message)
 
-data class SourceColumn(val title: String, val column: Int) {//source point
-    val locales: List<Locale> by lazy {
-        allLocales.items.filter { locale -> locale.identifiedBy(title) }
-    }
-}
-
-data class TargetDirectory(val file: File){
-    val path: Path = file.toPath()
-}
 
 data class ImportEvaluation(val projectType: ProjectType, val matchedSourcesAndTargets: MatchedSourcesAndTargets)
 
@@ -85,37 +44,6 @@ enum class ProjectType {
     abstract val translationKeyType: TranslationKeyType
 }
 
-data class MatchedSourceAndTarget(val sourceColumn: SourceColumn, val targetDirectory: TargetDirectory)//TransactionAddress, TransferAddress
-class MatchedSourcesAndTargets(private val _matches: MutableList<MatchedSourceAndTarget> = mutableListOf())
-    : Iterable<MatchedSourceAndTarget> by _matches {
-
-    val matches: List<MatchedSourceAndTarget> = _matches
-
-    val count: Int
-        get() = _matches.count()
-
-    fun add(matchedSourceAndTarget: MatchedSourceAndTarget) {
-        _matches.add(matchedSourceAndTarget)
-    }
-
-    fun notContainsSource(sourceColumn: SourceColumn): Boolean {
-        return !containsSource(sourceColumn)
-    }
-    private fun containsSource(sourceColumn: SourceColumn): Boolean {
-        return _matches.find { address -> address.sourceColumn.column==sourceColumn.column } != null
-    }
-
-    fun notContainsTarget(target: TargetDirectory): Boolean {
-        return !containsTarget(target)
-    }
-    private fun containsTarget(target: TargetDirectory): Boolean {
-        return _matches.find { address -> address.targetDirectory.file.toPath()==target.file.toPath() } != null
-    }
-
-    fun getAt(index: Int): MatchedSourceAndTarget {
-        return _matches[index]
-    }
-}
 data class ImportConfiguration(val keyColumn: Int,
                                val firstTranslationRow: Int,
                                val matchedSourcesAndTargets: MatchedSourcesAndTargets,
--- src/main/kotlin/SourceTargetMatching.kt
+++ src/main/kotlin/SourceTargetMatching.kt
@@ -1,3 +1,79 @@
+import Locales.Companion.allLocales
+import java.io.File
+import java.nio.file.Path
+import java.text.DateFormat
+import java.util.*
+
+fun Locale.identifiedBy(localeDataCandidate: String): Boolean {
+    return when (localeDataCandidate.toLowerCase()) {
+        this.country.toLowerCase() -> true
+        this.displayCountry.toLowerCase() -> true
+        this.language.toLowerCase() -> true
+        this.displayLanguage.toLowerCase() -> true
+        this.toLanguageTag().toLowerCase() -> true
+        else -> false
+    }
+}
+
+class Locales {
+
+    fun findLocale(localeDataCandidate: String): Locale? {
+        return items.find { locale -> locale.identifiedBy(localeDataCandidate) }
+    }
+
+    private constructor()
+
+    companion object {
+        val allLocales = Locales()
+    }
+
+    val items: Array<Locale> by lazy {
+        DateFormat.getAvailableLocales()
+    }
+}
+
+data class SourceColumn(val title: String, val column: Int) {//source point
+    val locales: List<Locale> by lazy {
+        allLocales.items.filter { locale -> locale.identifiedBy(title) }
+    }
+}
+
+data class TargetDirectory(val file: File){
+    val path: Path = file.toPath()
+}
+
+data class MatchedSourceAndTarget(val sourceColumn: SourceColumn, val targetDirectory: TargetDirectory)//TransactionAddress, TransferAddress
+class MatchedSourcesAndTargets(private val _matches: MutableList<MatchedSourceAndTarget> = mutableListOf())
+    : Iterable<MatchedSourceAndTarget> by _matches {
+
+    val matches: List<MatchedSourceAndTarget> = _matches
+
+    val count: Int
+        get() = _matches.count()
+
+    fun add(matchedSourceAndTarget: MatchedSourceAndTarget) {
+        _matches.add(matchedSourceAndTarget)
+    }
+
+    fun notContainsSource(sourceColumn: SourceColumn): Boolean {
+        return !containsSource(sourceColumn)
+    }
+    private fun containsSource(sourceColumn: SourceColumn): Boolean {
+        return _matches.find { address -> address.sourceColumn.column==sourceColumn.column } != null
+    }
+
+    fun notContainsTarget(target: TargetDirectory): Boolean {
+        return !containsTarget(target)
+    }
+    private fun containsTarget(target: TargetDirectory): Boolean {
+        return _matches.find { address -> address.targetDirectory.file.toPath()==target.file.toPath() } != null
+    }
+
+    fun getAt(index: Int): MatchedSourceAndTarget {
+        return _matches[index]
+    }
+}
+
 @Suppress("ClassName")
 class iOSSourceTargetMatcher: SourceTargetMatcher {
 
