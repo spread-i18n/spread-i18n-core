fde9a9b6e2c61607856f5c6ce56e4f2a0e7ba83e
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-05-01 08:46:57 +0200

    Move dir/file mocking to Mocking.kt

--- src/test/kotlin/Mocking.kt
+++ src/test/kotlin/Mocking.kt
@@ -4,6 +4,8 @@ import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellType
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Sheet
+import java.io.File
+import java.nio.file.Path
 
 fun mockSheet(sheetContent: String): Sheet {
     return mockk{
@@ -24,3 +26,36 @@ fun mockSheet(sheetContent: String): Sheet {
                         .iterator()
     }
 }
+
+class Dir(dirBlock: Dir.()->Unit) {
+    private val files = mutableListOf<File>()
+    private val dirs = mutableListOf<File>()
+
+    init {
+        dirBlock()
+    }
+
+    fun file(name: String): File {
+        val file = mockk<File>()
+        every { file.name } returns name
+        every { file.toPath() } returns Path.of(name)
+        every { file.isDirectory } returns false
+        files.add(file)
+        return file
+    }
+
+    fun dir(name: String, dirBlock: Dir.()->Unit): File {
+        val dir = mockk<File>()
+        every { dir.name } returns name
+        every { dir.toPath() } returns Path.of(name)
+        every { dir.isDirectory } returns true
+        every { dir.dirs } returns Dir(dirBlock).allDirs()
+        dirs.add(dir)
+        return dir
+    }
+
+    private fun allDirs() = dirs.toTypedArray()
+}
+
+fun dir(name: String, block: Dir.()->Unit): File = Dir(){}.dir(name, block)
+
