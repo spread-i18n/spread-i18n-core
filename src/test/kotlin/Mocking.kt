b96872cb54eebee63cd9f0b289372bc4e1ce39e0
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-05-06 19:10:16 +0200

    Use 'answers{}' instead of 'returns' to avoid 'iterator memory'

--- src/test/kotlin/Mocking.kt
+++ src/test/kotlin/Mocking.kt
@@ -1,7 +1,10 @@
+import internal.TargetDirectory
 import internal.dirs
 import internal.skipTo
+import io.mockk.MockKSettings.relaxed
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.CellType
 import org.apache.poi.ss.usermodel.Row
@@ -10,22 +13,34 @@ import java.io.File
 import java.nio.file.Path
 
 fun mockSheet(sheetContent: String): Sheet {
+
+    fun mockCells(rowContent: String):List<Cell> {
+        return rowContent.split("│").asSequence().skipTo(1).map { cellContent ->
+            val cell = mockk<Cell>()
+            every { cell.stringCellValue } returns cellContent.trim()
+            every { cell.cellType } returns CellType.STRING
+            cell
+        }.toList()
+    }
+
     return mockk{
-        every { rowIterator() } returns
-                sheetContent.lineSequence()
-                        .map { it.trim() }
-                        .filter { it.startsWith("│") }
-                        .map {
-                            val row = mockk<Row>()
-                            every { row.cellIterator() } returns it.split("│").asSequence().skipTo(1).map { cellContent ->
-                                val cell = mockk<Cell>()
-                                every { cell.stringCellValue } returns cellContent
-                                every { cell.cellType } returns CellType.STRING
-                                cell
-                            }.iterator()
-                            row
+        every { rowIterator() } answers {
+            sheetContent.lineSequence()
+                    .map { it.trim() }
+                    .filter { it.startsWith("│") }
+                    .map { rowContent ->
+                        println("Row:$rowContent")
+                        val row = mockk<Row>()
+                        every { row.cellIterator() } returns mockCells(rowContent).iterator()
+
+                        val index = slot<Int>()
+                        every { row.getCell(capture(index)) } answers {
+                            mockCells(rowContent)[index.captured]
                         }
-                        .iterator()
+                        row
+                    }
+                    .iterator()
+        }
     }
 }
 
@@ -61,3 +76,4 @@ class Dir(dirBlock: Dir.()->Unit) {
 
 fun dir(name: String, block: Dir.()->Unit): File = Dir(){}.dir(name, block)
 
+internal fun String.asTargetDir() = TargetDirectory(File(this))
