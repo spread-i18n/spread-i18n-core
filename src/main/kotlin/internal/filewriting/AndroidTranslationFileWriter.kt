ec15d9aa96bbdc69bffec2356db10a5e4aa71e41
Robert Andrzejczyk <robert.andrzejczyk@gmail.com>
2022-05-06 21:33:17 +0200

    Implement Android Translation File Writer

---
+++ src/main/kotlin/internal/filewriting/AndroidTranslationFileWriter.kt
@@ -0,0 +1,13 @@
+package internal.filewriting
+
+import java.nio.file.Path
+
+class AndroidTranslationFileWriter(private val targetDirectoryPath: Path) : TranslationFileWriter {
+    override fun write(key: String, value: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+}
